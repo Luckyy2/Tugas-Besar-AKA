5797086fd5a5dd908d97bba71eecd3a419814507
Luckyy2 <bimolp@student.telkomuniversity.ac.id>
2024-12-31 14:11:31 +0700

    Penambahan Runing Time

--- Iteratif.java
+++ Iteratif.java
@@ -15,6 +15,9 @@ public class IterativeMaxStock {
             stokBuku[i] = scanner.nextInt();
         }
 
+        // Mulai menghitung waktu eksekusi
+        long startTime = System.nanoTime();
+
         // Mencari stok terbanyak
         int maxIndex = 0; // Indeks untuk stok maksimum
         for (int i = 1; i < stokBuku.length; i++) {
@@ -23,9 +26,17 @@ public class IterativeMaxStock {
             }
         }
 
+        // Menghentikan penghitungan waktu eksekusi
+        long endTime = System.nanoTime();
+
         // Menampilkan hasil
         System.out.println("Jenis buku dengan stok terbanyak: " + jenisBuku[maxIndex]);
         System.out.println("Jumlah stok: " + stokBuku[maxIndex]);
+
+        // Menghitung dan menampilkan waktu eksekusi
+        long executionTime = endTime - startTime;
+        System.out.println("Waktu eksekusi (dalam nanodetik): " + executionTime + " ns");
+        System.out.println("Waktu eksekusi (dalam milidetik): " + (executionTime / 1_000_000.0) + " ms");
 
         scanner.close();
     }
--- rekursif.java
+++ rekursif.java
@@ -15,12 +15,23 @@ public class RecursiveMaxStock {
             stokBuku[i] = scanner.nextInt();
         }
 
+        // Mulai menghitung waktu eksekusi
+        long startTime = System.nanoTime();
+
         // Mencari stok terbanyak dengan rekursi
         int maxIndex = findMaxIndex(stokBuku, 0, 0);
+
+        // Menghentikan penghitungan waktu eksekusi
+        long endTime = System.nanoTime();
 
         // Menampilkan hasil
         System.out.println("Jenis buku dengan stok terbanyak: " + jenisBuku[maxIndex]);
         System.out.println("Jumlah stok: " + stokBuku[maxIndex]);
+
+        // Menghitung dan menampilkan waktu eksekusi
+        long executionTime = endTime - startTime;
+        System.out.println("Waktu eksekusi (dalam nanodetik): " + executionTime + " ns");
+        System.out.println("Waktu eksekusi (dalam milidetik): " + (executionTime / 1_000_000.0) + " ms");
 
         scanner.close();
     }
